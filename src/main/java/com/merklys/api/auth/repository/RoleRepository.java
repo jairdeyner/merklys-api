8cc83428c0647168250e62b50c3492d484ecd9e5
jairdeyner <jairdeyner730791@gmail.com>
2026-04-10 18:27:02 -0500

    feat(auth/role): add existsByNameIgnoreCase method

--- src/main/java/com/merklys/api/auth/repository/RoleRepository.java
+++ src/main/java/com/merklys/api/auth/repository/RoleRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.merklys.api.auth.entity.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-
+    boolean existsByNameIgnoreCase(String name);
 }
