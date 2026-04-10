d9fbeaebfffe01630b6d1844fddb77f947700b36
jairdeyner <jairdeyner730791@gmail.com>
2026-04-10 18:37:52 -0500

    feat(auth/role): add role controller endpoints

---
+++ src/main/java/com/merklys/api/auth/controller/RoleController.java
@@ -0,0 +1,62 @@
+package com.merklys.api.auth.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.merklys.api.auth.dto.request.RoleRequest;
+import com.merklys.api.auth.dto.response.RoleResponse;
+import com.merklys.api.auth.service.RoleService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/roles")
+public class RoleController {
+
+    private final RoleService roleService;
+
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RoleResponse>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(this.roleService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RoleResponse> findById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.roleService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<RoleResponse> create(@Valid @RequestBody RoleRequest roleRequest) {
+        RoleResponse response = this.roleService.create(roleRequest);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
+
+        return ResponseEntity.status(HttpStatus.CREATED).location(location).body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<RoleResponse> update(@PathVariable Long id, @Valid @RequestBody RoleRequest roleRequest) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.roleService.update(id, roleRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        this.roleService.delete(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+}
